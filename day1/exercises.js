87cb391e1704152b18b37151ca982ac130bdfbc3
kuma <daniel099d@gmail.com>
2022-04-26 02:05:45 +0700

    update exercises

---
+++ day1/exercises.js
@@ -0,0 +1,35 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+
+/*comments can make code readable, 
+easy to reuse and informative */
+
+let country = "Indonesia"
+let isMarried = "false"
+let job
+console.log(country, isMarried, job, null)
+
+console.log(typeof country)
+console.log(typeof false)
+console.log(typeof job)
+console.log(typeof null)
+
+let fisrtName
+let lastName
+let age
+console.log(fisrtName, lastName, age, job)
+
+let yearNow = 2022
+let say = "Hello"
+console.log(say, country, yearNow, true)
+
+console.log(say)
+console.log(country)
+console.log(yearNow)
+
+console.log(say, country, yearNow)
+
+let myAge = 20
+let yourAge = 25
+console.log("I am ", myAge, " years old")
+console.log("You are ", yourAge, " years old")
